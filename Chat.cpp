da37cba1fe7a1540a78248e0a9c0967a08e085f7
MaximAhmedov <ahmedowmaksim1997@gmail.com>
2023-11-28 22:04:45 +0300

    first for try HT

--- Chat.cpp
+++ Chat.cpp
@@ -2,9 +2,7 @@
 #include "iostream"
 #include "string.h"
 
-Chat::Chat() {
-    data_count = 0;
-}
+
 void Chat::reg(char _login[LOGINLENGTH], char _pass[], int pass_length) {
     data[data_count++] = AuthData(_login, sha1(_pass, pass_length));
 }
@@ -29,4 +27,68 @@ bool Chat::login(char _login[LOGINLENGTH], char _pass[], int pass_length) {
     delete[] digest;
 
     return cmpHashes;
+}
+
+Chat::HashTable::HashTable()
+{
+    count = 0;
+    mem_size = 8;
+    array = new Pair[mem_size];
+}
+
+Chat::HashTable::~HashTable()
+{
+    delete[] array;
+}
+
+void Chat::HashTable::add(char _login[LOGINLENGTH], uint* sh1)
+{
+    int index = -1, i = 0;
+    // берем пробы по всем i от 0 до размера массива
+    for (; i < mem_size; i++) {
+        index = hash_func(_login, i);
+        if (array[index].status == enPairStatus::free) {
+            // найдена пустая ячейка, занимаем ее
+            break;
+        }
+    }
+
+    if (i >= mem_size) {
+        resize();
+        add(_login, sh1);
+    }
+    else {
+        array[index] = Pair(_login, sh1);
+        count++;
+    }
+}
+
+void Chat::HashTable::resize()
+{
+    Pair* save = array;
+    int save_ms = mem_size;
+
+    mem_size *= 2;
+    array = new Pair[mem_size];
+    count = 0;
+
+    for (int i = 0; i < save_ms; i++) {
+        Pair& old_pair = save[i];
+        if (old_pair.status == enPairStatus::engaged) {
+            add(old_pair.login, old_pair.pass_sha1_hash);
+        }
+    }
+
+    delete[] save;
+}
+
+int Chat::HashTable::hash_func(char _login[LOGINLENGTH], int offset)
+{
+    const double A = 0.7;
+    int sum = 0, i = 0;
+    for (; i < strlen(_login); i++) {
+        sum += _login[i];
+    }
+    // квадратичные пробы
+    return int((mem_size + offset * offset) * (sum * A - int(sum * A)));
 }
--- Chat.h
+++ Chat.h
@@ -8,38 +8,57 @@
 
 class Chat {
 public:
-    Chat();
+    Chat() = default;
     void reg(char _login[LOGINLENGTH], char _pass[], int pass_length);
     bool login(char _login[LOGINLENGTH], char _pass[], int pass_length);
 
 private:
-    struct AuthData {
-        AuthData() :
-            login(""),
-            pass_sha1_hash(0) {
-        }
-        ~AuthData() {
-            if (pass_sha1_hash != 0)
-                delete[] pass_sha1_hash;
-        }
-        AuthData(char _login[LOGINLENGTH], uint* sh1) {
-            strcpy(login, _login);
-            pass_sha1_hash = sh1;
-        }
-        AuthData& operator = (const AuthData& other) {
-            strcpy(login, other.login);
+    class HashTable { // хеш-таблица
+    public:
+        HashTable();
+        ~HashTable();
+    private:
+        enum enPairStatus {
+            free, // свободен
+            engaged, //зан€т
+            deleted //удален
+        };
+        struct Pair { // пара ключ-значение
+            Pair() : // конструктор по умолчанию
+                login(""),
+                pass_sha1_hash(0),
+                status(enPairStatus::free) {
 
-            if (pass_sha1_hash != 0)
-                delete[] pass_sha1_hash;
-            pass_sha1_hash = new uint[SHA1HASHLENGTHUINTS];
+            }
+            ~Pair() {
+                if (pass_sha1_hash != 0)
+                    delete[] pass_sha1_hash;
+            }
+            Pair(char _login[LOGINLENGTH], uint* sh1) : status(enPairStatus::engaged) {
+                strcpy(login, _login);
+                pass_sha1_hash = sh1;
+            }
+            Pair& operator = (const Pair& other) {
+                strcpy(login, other.login);
+                status = other.status;
+                if (pass_sha1_hash != 0)
+                    delete[] pass_sha1_hash;
+                pass_sha1_hash = new uint[SHA1HASHLENGTHUINTS];
 
-            memcpy(pass_sha1_hash, other.pass_sha1_hash, SHA1HASHLENGTHBYTES);
+                memcpy(pass_sha1_hash, other.pass_sha1_hash, SHA1HASHLENGTHBYTES);
 
-            return *this;
-        }
-        char login[LOGINLENGTH];
-        uint* pass_sha1_hash;
+                return *this;
+            }
+            char login[LOGINLENGTH];
+            uint* pass_sha1_hash;
+            enPairStatus status; // состо€ние €чейки
+        };
+        void add(char _login[LOGINLENGTH], uint* sh1);
+        void resize();
+        int hash_func(char _login[LOGINLENGTH], int offset);
+
+        Pair* array;
+        int mem_size;
+        int count;
     };
-    AuthData data[SIZE];
-    int data_count;
 };
